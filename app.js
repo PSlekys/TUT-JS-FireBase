a78e1109b0772f0df57c9f1ed6454f90017c6832
PSlekys <petras@mozilla.lt>
2019-08-14 15:29:40 +0300

    Finalised auth, menu

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const auth = firebase.auth();
-
 auth.onAuthStateChanged(user => {
+  const signUpLink = document.getElementById("sign-up-link");
+  const signOutLink = document.getElementById("sign-out-link");
+  const signInLink = document.getElementById("sign-in-link");
+  const hotelLink = document.getElementById("hotel-link");
+
   if (user) {
+    signUpLink.style.display = "none";
+    signInLink.style.display = "none";
+    signOutLink.addEventListener("click", () => {
+      auth.signOut();
+    });
+
+    if (location.pathname.includes("hotel")) {
+      hotelLink.style.fontWeight = "900";
+    }
   } else {
+    if (location.pathname.includes("hotel")) {
+      location.href = "./signin.html";
+    }
+    signOutLink.style.display = "none";
+    hotelLink.style.display = "none";
+    if (location.pathname.includes("signup")) {
+      signUpLink.style.fontWeight = "bold";
+    } else if (location.pathname.includes("signin")) {
+      signInLink.style.fontWeight = "bold";
+    }
   }
 });
